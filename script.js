cbc2cc2524de38af87c4027aa010db5e7ebf15d7
Joke Heyndels <jo_hannah@outlook.com>
2020-04-05 13:30:18 +0200

    Format script

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const speed = document.querySelector(".speed")
-const bar = speed.querySelector(".speed-bar")
-const video = document.querySelector(".flex")
+const speed = document.querySelector(".speed");
+const bar = speed.querySelector(".speed-bar");
+const video = document.querySelector(".flex");
 
 const min = 0.4;
-    const max = 4
+const max = 4;
 
- function handleMove(e) {
-    const y = e.pageY - this.offsetTop
-    const percent = y / this.offsetHeight
-    const height = Math.round(percent * 100) + "%"
-    const playbackRate = percent * (max - min) + min
-    bar.style.height = height
-    bar.textContent = playbackRate.toFixed(2) + "x"
-    video.playbackRate = playbackRate
+function handleMove(e) {
+  const y = e.pageY - this.offsetTop;
+  const percent = y / this.offsetHeight;
+  const height = Math.round(percent * 100) + "%";
+  const playbackRate = percent * (max - min) + min;
+  bar.style.height = height;
+  bar.textContent = playbackRate.toFixed(2) + "x";
+  video.playbackRate = playbackRate;
 }
 
 function keyControl(e) {
-    let playbackRate = video.playbackRate
-    const changeBy = 0.1
-    if (e.key === "ArrowUp") {
-        if (playbackRate < max - changeBy) {
-            playbackRate += changeBy
-        }
-    } else if (e.key === "ArrowDown") {
-        if (playbackRate > min + changeBy) {
-            playbackRate -= changeBy
-        }
+  let playbackRate = video.playbackRate;
+  const changeBy = 0.1;
+  if (e.key === "ArrowUp") {
+    if (playbackRate < max - changeBy) {
+      playbackRate += changeBy;
     }
-    video.playbackRate = playbackRate
-    bar.textContent = playbackRate.toFixed(2) + "x"
-    bar.style.height = (playbackRate * 100 / max) + "%" 
+  } else if (e.key === "ArrowDown") {
+    if (playbackRate > min + changeBy) {
+      playbackRate -= changeBy;
+    }
+  }
+  video.playbackRate = playbackRate;
+  bar.textContent = playbackRate.toFixed(2) + "x";
+  bar.style.height = (playbackRate * 100) / max + "%";
 }
 
-speed.addEventListener("mousemove", handleMove)
-speed.addEventListener("touchchange", handleMove)
-window.addEventListener("keydown", keyControl)
+speed.addEventListener("mousemove", handleMove);
+speed.addEventListener("touchchange", handleMove);
+window.addEventListener("keydown", keyControl);
